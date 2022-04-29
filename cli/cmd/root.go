7ba7c80fec08cc3b8c6bcad20bc176da7023a78f
marjamis <marjamis>
2022-04-29 10:36:37 +1000

    refactor(cli): Cleaned up the root cmd to be more granular and easier for future changes, if required

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"text/tabwriter"
+	"time"
 
 	"github.com/marjamis/docket/cli/internal/pkg/formatting"
 	"github.com/marjamis/docket/lambda/reader/pkg/event"
@@ -16,11 +17,12 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
 )
 
 var (
-	cfgFile   string
-	startFrom string
+	cfgFile string
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -28,45 +30,60 @@ var rootCmd = &cobra.Command{
 	Use:   "docket",
 	Short: "Pull stored CloudWatch Events for ECS for quick reference ECS timelines/activities",
 	Run: func(cmd *cobra.Command, args []string) {
-		settingsPrint()
-
-		dynamodb_svc := dynamodb.New(session.New(&aws.Config{
-			Region: aws.String(viper.GetString("Resources.DynamoDB.Region")),
-		}))
-		// xray.AWS(dynamodb_svc.Client)
-
-		scanOutput, err := dynamodb_svc.Scan(&dynamodb.ScanInput{
-			TableName:        aws.String(viper.GetString("Resources.DynamoDB.TableName")),
-			FilterExpression: aws.String("begins_with(#n0, :v0)"),
-			ExpressionAttributeNames: map[string]*string{
-				"#n0": aws.String("time"),
-			},
-			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-				":v0": {
-					S: aws.String("2022-01-06"),
-				},
-			},
-		})
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// fmt.Printf("%+v", scanOutput)
-
-		var events []event.EventData
-		dynamodbattribute.UnmarshalListOfMaps(scanOutput.Items, &events)
-
-		w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', tabwriter.DiscardEmptyColumns)
-		for i, v := range events {
-			fmt.Fprintf(w, "%d\t%s\n", i, formatting.FormatEventData(&v))
-		}
-		w.Flush()
+		printOutput(scan())
 	},
 }
 
-func settingsPrint() {
-	// TODO make prettier
-	fmt.Printf("Table settings:\n* TableName: %s\n* Region: %s\n", viper.Get("Resources.DynamoDB.TableName"), viper.Get("Resources.DynamoDB.Region"))
+func scan() *dynamodb.ScanOutput {
+	ddbClient := dynamodb.New(session.New(&aws.Config{
+		Region: aws.String(viper.GetString("Resources.DynamoDB.Region")),
+	}))
+
+	scanOutput, err := ddbClient.Scan(generateQuery())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return scanOutput
+}
+
+func generateQuery() *dynamodb.ScanInput {
+	input := &dynamodb.ScanInput{
+		TableName:        aws.String(viper.GetString("Resources.DynamoDB.TableName")),
+		FilterExpression: aws.String("begins_with(#n0, :v0)"),
+		ExpressionAttributeNames: map[string]*string{
+			"#n0": aws.String("time"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":v0": {
+				S: aws.String(viper.GetString("startFrom")),
+			},
+		},
+	}
+
+	log.Debugf("%+v", input)
+
+	return input
+}
+
+// TODO fix the entire output to look nicer and fix the formatters
+func printOutput(scanOutput *dynamodb.ScanOutput) {
+	if *scanOutput.Count == int64(0) {
+		log.Printf("No data in table. Exiting...")
+		return
+	}
+
+	var events []event.EventData
+
+	dynamodbattribute.UnmarshalListOfMaps(scanOutput.Items, &events)
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', tabwriter.DiscardEmptyColumns)
+	fmt.Fprint(w, "Number\tDate\tEvent Type\tARN\tDetail Type\tEvent Id\n")
+
+	for i, v := range events {
+		fmt.Fprintf(w, "%d\t%s\n", i, formatting.FormatEventData(&v))
+	}
+	w.Flush()
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -81,7 +98,9 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.docket.yaml)")
-	rootCmd.Flags().StringVarP(&startFrom, "startFrom", "s", "sads", "Time from which to start the docker timeline")
+
+	var flag string
+	rootCmd.Flags().StringVarP(&flag, "startFrom", "s", time.Now().UTC().Format("2006-01-02"), "Time from which to start the docker timeline")
 	viper.BindPFlag("startFrom", rootCmd.Flags().Lookup("startFrom"))
 }
 
